10e415ae963fc7b901fab258bc6fcce104aa4a93
ferminmartel <ferminmartel@gmail.com>
2025-05-06 18:57:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Scroll suave al hacer clic en los enlaces internos
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  });
+});
